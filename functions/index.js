d6dbdfcd8b6d810c59b157f0244537cd63d61a05
code-ankush <code.ankush@gmail.com>
2017-08-15 11:20:00 +0530

    changing version of node will solving the problem of buffer allocation

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
+const app = express();
+
+app.get('/projects', function (req, res) {
+  res.send('Hello World!')
+})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+
+exports.projects = functions.https.onRequest(app);
